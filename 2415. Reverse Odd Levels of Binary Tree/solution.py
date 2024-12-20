2e3e6294f52cccb79fd3eeeb5af2cb10b38a353a
Ayush Singh <ayushsingh2999@gmail.com>
2024-12-20 17:01:36 +0530

    new  solution

--- 2415. Reverse Odd Levels of Binary Tree/solution.py
+++ 2415. Reverse Odd Levels of Binary Tree/solution.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+#using bfs
 class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         queue = deque([root])
@@ -30,4 +31,19 @@ class Solution:
         return root
 
 
+    #using dfs
+    class Solution:
+    def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        def dfs(left: Optional[TreeNode], right: Optional[TreeNode], level: int):
+            if not left or not right:
+                return 
+            if level % 2 == 1:
+                left.val,right.val = right.val,left.val
+                
+            dfs(left.left , right.right,level+1)
+            dfs(left.right , right.left,level+1)  
+        if root:
+            dfs(root.left,root.right,1)
         
+        return root    
